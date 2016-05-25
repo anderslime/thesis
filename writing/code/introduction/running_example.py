cbb9f7c219bc445263392f4620a26882cb3698ea
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-05-25 11:42:22 +0200

    Revised code examples to be more clear

--- writing/code/introduction/running_example.py
+++ writing/code/introduction/running_example.py
@@ -1,11 +1,11 @@
 \begin{minted}[linenos]{python}
 def course_score(course)
-	participants = ParticipantDB.find_by_course(course)
-	total_score = 0
-	for participant in participants:
-		total_score += participant_score(participant)
-	return total_score / len(participants)
+    participants = ParticipantDB.find_by_course(course)
+    total_score = 0
+    for participant in participants:
+        total_score += participant_score(participant)
+    return total_score / len(participants)
 
-def participant_score(participant):
-	return numpy.advanced_statistical_method(participant)
+def time_consuming_participant_score(participant):
+    return numpy.advanced_statistical_method(participant)
 \end{minted}
--- writing/code/manual_trigger_invalidation.py
+++ writing/code/manual_trigger_invalidation.py
@@ -6,18 +6,18 @@ def cache_key_for_participant_score(participant):
     cache_key_components = [
         'cached_participant_score',
         participant.type,
-        participant.id,
-        participant.update_timestamp
+        participant.id
     ]
     return '/'.join(cache_key_components)
 
-def cached_participant_score(participant):
+def cached_time_consuming_function(participant):
     """
     A function where the time consuming part is cached.
     The time_consuming_function is only called if it
     is not already cached.
     """
-    cache_key = cached_participant_score(participant)
+    cache_key = cache_key_for_participant_score(participant)
+
     if is_present_in_cache(cache_key):
         return fetch_from_cache(cache_key)
     else:
@@ -32,8 +32,17 @@ participant = ParticipantDB.load_one_from_database
 print cached_participant_score(participant)
 
 # This second time it is called, the return value for
-# the time_consuming_participant_score cached, which
-# means it returns the value from the cache
+# the time_consuming_participant_score is cached, which means it
+# returns the value from the cache
+print cached_participant_score(participant)
+
+# Now we invalidate the cached value
+cache_key = cache_key_for_participant_score(participant)
+invalidate_cached_value(cache_key)
+
+# Since the value has been invalidated
+# the cached_time_consuming_function will recalculate
+# the result when called at this point
 print cached_participant_score(participant)
 
 \end{minted}
