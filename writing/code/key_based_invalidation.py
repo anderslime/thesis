8a29c0afc0728f05be4fdbd40ca83b63af1808ee
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-05-25 14:19:48 +0200

    This is just so great

--- writing/code/key_based_invalidation.py
+++ writing/code/key_based_invalidation.py
@@ -12,13 +12,9 @@ def cache_key_for_participant_score(participant):
     return '/'.join(cache_key_components)
 
 def cached_participant_score(participant):
-    """
-    A function where the time consuming part is cached.
-    The time_consuming_function is only called if it
-    is not already cached.
-    """
+
     cache_key = cached_participant_score(participant)
-    if is_present_in_cache(cache_key):
+    if is_fresh_in_cache(cache_key):
         return fetch_from_cache(cache_key)
     else:
         result = time_consuming_participant_score(participant)
@@ -35,5 +31,4 @@ print cached_participant_score(participant)
 # the time_consuming_participant_score cached, which
 # means it returns the value from the cache
 print cached_participant_score(participant)
-
 \end{minted}
