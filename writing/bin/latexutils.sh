9a19f882e0f26759f7fc3323313da2e06994430a
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-06-16 15:22:32 +0200

    NEW CLI with warning notification - cool procrastination

--- writing/bin/latexutils.sh
+++ writing/bin/latexutils.sh
@@ -7,11 +7,16 @@ function compile_latex_silently {
 }
 
 function output_latex_status {
-  if [ $? -eq 0 ]
+  has_compiled=$?
+  has_warnings=$(analyze_texlog)
+  if [ "$has_compiled" -ne 0 ]
   then
-    show_finished_notification
-  else
     show_error_notification
+  elif [ "$has_warnings" ]
+  then
+    show_warning_notification
+  else
+    show_finished_notification
   fi
 }
 
@@ -20,18 +25,29 @@ function show_compiling_notification {
   terminal-notifier -title 'Compiling your LaTeX' -message '🤓 🚀 '
 }
 
-function show_finished_notification {
-  echo "Done!"
-  analyze_texlog
-  terminal-notifier -execute "open ~/thesis/writing/thesis.pdf" -title '✅ Your PDF is ready!' -message 'Click here to see it 📄 '
-}
-
 function show_error_notification {
   echo "Error!"
   analyze_texlog
   terminal-notifier -title '‼️  Error on LaTeX compilation!' -message 'Your document was not compiled ‼️ 🙃 '
 }
 
+function show_warning_notification {
+  echo "Compiled with Warnings!"
+  analyze_texlog
+  terminal-notifier -title "⚠️  Warnings" -message "Compiled, but with warnings 😥"
+}
+
+function show_finished_notification {
+  echo "Done!"
+  analyze_texlog
+  terminal-notifier -execute "open ~/thesis/writing/thesis.pdf" -title '✅ Your PDF is ready!' -message 'Click here to see it 📄 '
+}
+
 function analyze_texlog {
   ~/thesis/writing/bin/texlog_extract --config=.texlog_config thesis.log
+}
+
+function has_warnings {
+  analyze_texlog > /dev/null
+  return [ $? -eq 0 ]
 }
