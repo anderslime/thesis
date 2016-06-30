7b30185013850f8e34a40539bec04496d910825c
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-06-30 14:11:05 +0200

    Remove linebreak

--- writing/code/manual_trigger_invalidation.py
+++ writing/code/manual_trigger_invalidation.py
@@ -13,7 +13,6 @@ def cache_key_for_participant_score(participant):
 
 def cached_time_consuming_function(participant):
     cache_key = cache_key_for_participant_score(participant)
-
     if is_fresh_in_cache(cache_key):
         return fetch_from_cache(cache_key)
     else:
