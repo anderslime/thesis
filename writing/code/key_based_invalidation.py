5ae9a84f9124a8e5b3d82cbf1a06ea52f3a3986a
Anders Emil Nielsen <aemilnielsen@gmail.com>
2016-06-30 14:07:52 +0200

    WOHO - caching approaches correction

--- writing/code/key_based_invalidation.py
+++ writing/code/key_based_invalidation.py
@@ -13,7 +13,6 @@ def cache_key_for_participant_score(participant):
     return '/'.join(cache_key_components)
 
 def cached_participant_score(participant):
-
     cache_key = cached_participant_score(participant)
     if is_fresh_in_cache(cache_key):
         return fetch_from_cache(cache_key)
@@ -29,7 +28,6 @@ participant = ParticipantDB.load_one_from_database
 print cached_participant_score(participant)
 
 # This second time it is called, the return value for
-# the time_consuming_participant_score cached, which
-# means it returns the value from the cache
+# the time_consuming_participant_score cached
 print cached_participant_score(participant)
 \end{minted}
